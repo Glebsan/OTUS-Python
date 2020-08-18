f8fb32f4f49aaa8f3020da6c54740e33ce731c12
Glebsan <g.shishlyakov@gmail.com>
2020-08-18 11:50:49 +0300

    power def solution with map add def number solution

--- month_1_lesson_3/tasks_01_03.py
+++ month_1_lesson_3/tasks_01_03.py
@@ -1,3 +1,4 @@
+import itertools
 # def power(a, p=2):
 #     """
 #     Написать функцию, которая принимает N целых чисел и возвращает список квадратов эих чисел. Бонусом будет сделать keyword аргумент для выбора степени, в которую будут возводиться числа.
@@ -17,6 +18,36 @@ def power(a, p=2):
     answ = [i ** p for i in a]
     return answ
 
+def power(a, p=2):
+    """РЕШЕНИЕ ЧЕРЕЗ MAP
+       :param a: список целых чисел
+       :param p: степень
+       :return: возвращает ноый список
+       """
+    return list(map(pow, a, itertools.repeat(p)))
+
+def prime_num(a):
+    """ПРОВЕРКА НА ПРОСТОЕ ЧИСЛО True/False"""
+    i = 2
+    while a > 1 and i * i < a and a % i != 0:
+        i += 1
+    return i * i > a
+
+ODD = 'odd'
+EVEN = 'even'
+SIMPLE = 'simple'
+
+
+def numbers(a, b = EVEN):
+    if b == 'even':
+        answ = list(filter(lambda x: x % 2 == 0, a))
+    elif b == 'odd':
+       answ = list(filter(lambda x: x % 2 != 0, a))
+    elif b == 'simple':
+        answ = list(filter(prime_num, a))
+    return answ
+
+
 
 test = [1, 2, 3, 4, 5]
 
@@ -24,3 +55,8 @@ print("Написать функцию, которая принимает N це
 print('Функция принимает на вход список:', test, 'и возводит в степень.(по-умолчанию "2"):', power(test))
 print('Функция принимает на вход список:', test, 'и возводит в степень(4) указанную во втором аргументе функции:',
       power(test, 4))
+print()
+print("Написать функцию, которая на вход принимает список из целых чисел, и возвращает только чётные/нечётные/простые числа")
+print("Тоько четные числа (по-умолчанию):",numbers(test))
+print("Тоько нечетные числа:",numbers(test, ODD))
+print("Тоько простые числа:",numbers(test, SIMPLE))
